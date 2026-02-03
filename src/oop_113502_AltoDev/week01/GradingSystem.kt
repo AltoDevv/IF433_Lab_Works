141855d349f1e24c6a4eb2f073eac1d9380ea5b5
AltoDev <hitokawiswara@gmail.com>
2026-02-03 15:38:03 +0700

    week01: initial setup and procedural code

--- src/oop_113502_AltoDev/week01/GradingSystem.kt
+++ src/oop_113502_AltoDev/week01/GradingSystem.kt
@@ -1,23 +1,8 @@
 package oop_113502_AltoDev.week01
 
-fun main() {
-    val name = "Alto Dev"
-    val score = 80
+fun main (){
+    var name: String = "John Thor"
+    var score: Int = 80
 
-    println("Nama: $name, Nilai: $score")
-    val grade = when (score){
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-    println("Grade kamu: $grade")
-    println("Status: ${calculateStatus(score)}")
-
-    val studentId: String? = null
-    val idLength = studentId?.length ?:0
-    println("panjang ID: $idLength")
-    
+    println("Name: "+name+", Nilai: "+score)
 }
-
-fun calculateStatus(score: Int) = if (score > 75) "lulus" else "tidak lulus"
