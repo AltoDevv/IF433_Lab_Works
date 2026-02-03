b828859f60f35a0b93ee6782913382b41d92e628
AltoDev <hitokawiswara@gmail.com>
2026-02-03 15:39:47 +0700

    week01: refactor to use val and string templates

--- src/oop_113502_AltoDev/week01/GradingSystem.kt
+++ src/oop_113502_AltoDev/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_113502_AltoDev.week01
 
 fun main (){
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name = "John Thor"
+    val score = 80
 
-    println("Name: "+name+", Nilai: "+score)
+    println("Name: $name, Nilai: $score")
 }
