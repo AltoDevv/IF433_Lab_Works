08d2bcf6cfdc41451268da4fd789400c82302533
AltoDev <hitokawiswara@gmail.com>
2026-02-06 12:27:56 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_113502_AltoDev/week01/GameStore.kt
+++ src/oop_113502_AltoDev/week01/GameStore.kt
@@ -10,25 +10,34 @@ fun calculateDiscount(price: Int): Int =
 fun printReceipt(
     title: String,
     originalPrice: Int,
-    finalPrice: Int
+    finalPrice: Int,
+    note: String
 ) {
     println("===== SteamKW Digital Receipt =====")
     println("Judul Game   : $title")
     println("Harga Asli   : Rp $originalPrice")
     println("Harga Akhir  : Rp $finalPrice")
+    println("Catatan      : $note")
 }
+
 
 fun main() {
 
     val gameTitle = "Cyberpunk 2077"
     val price = 750000
 
+    val userNote: String? = null
+
     val finalPrice = calculateDiscount(price)
+
+    val noteToPrint = userNote ?: "Tidak ada catatan"
 
     printReceipt(
         title = gameTitle,
         originalPrice = price,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        note = noteToPrint
     )
 
 }
+
