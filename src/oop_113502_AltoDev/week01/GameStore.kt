3ee9c2ca7a7c51632a54d3414a2c7cc9e2e228ad
AltoDev <hitokawiswara@gmail.com>
2026-02-06 12:26:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_113502_AltoDev/week01/GameStore.kt
+++ src/oop_113502_AltoDev/week01/GameStore.kt
@@ -1,5 +1,12 @@
 package oop_113502_AltoDev.week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
+
 fun main(){
     val gameTitle = "aDev 2026"
     val price = 750000
