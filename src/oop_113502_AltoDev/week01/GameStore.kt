ac8a3eb7a866b397f06594b085f355c8733b2fa2
AltoDev <hitokawiswara@gmail.com>
2026-02-06 12:16:34 +0700

    weel01: (tugas 2) implement receipt printing

--- src/oop_113502_AltoDev/week01/GameStore.kt
+++ src/oop_113502_AltoDev/week01/GameStore.kt
@@ -6,7 +6,22 @@ fun calculateDiscount(price: Int): Int =
     }else {
         price - (price * 10 / 100)
     }
+
+fun printReceipt(
+    title: String,
+    originalPrice: Int,
+    finalPrice: Int
+){
+    println("===== SteamKW Digital Receipt =====")
+    println("Judul Game : $title")
+    println("Harga Asli : RP $originalPrice")
+    println("Harga Akhir : RP $finalPrice")
+}
 fun main(){
     val gameTitle ="aDev 2026"
     val price = 750000
+
+    val finalPrice = calculateDiscount(price)
+
+    printReceipt(gameTitle, price, finalPrice)
 }
