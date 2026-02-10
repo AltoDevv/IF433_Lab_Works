930b651fba451c9a6a03b13d698c0146a545283e
AltoDev <hitokawiswara@gmail.com>
2026-02-10 15:47:27 +0700

    week02: (task 1) create loan class with default args

---
+++ src/oop_113502_AltoDev_week02/Loan.kt
@@ -0,0 +1,18 @@
+package oop_113502_AltoDev_week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+
+    fun calculateFine(): Int {
+
+        if (loanDuration > 3) {
+            return (loanDuration - 3) * 2000
+        }
+
+        return 0
+    }
+
+}
