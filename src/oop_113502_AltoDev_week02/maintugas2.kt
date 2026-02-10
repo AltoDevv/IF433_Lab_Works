ede9a354cf1e4e35c1c5a61fc7d2d54cbfe88318
AltoDev <hitokawiswara@gmail.com>
2026-02-10 16:01:32 +0700

    week02 : (task 2) implement the game

---
+++ src/oop_113502_AltoDev_week02/maintugas2.kt
@@ -0,0 +1,37 @@
+package oop_113502_AltoDev_week02
+
+import java.util.Scanner
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Masukkan nama hero: ")
+    val heroName = scanner.nextLine()
+
+    println("Mmasukkan base damage hero: ")
+    val heroDamage = scanner.nextLine()
+
+    val hero = Hero(heroName,heroDamage)
+
+    var enemyHp = 100
+
+    while (hero.isAlive() && enemyHp > 0) {
+        println("1. Serang")
+        println("2. Kabur")
+
+        val choice = scanner.nextInt()
+
+        if (choice == 1) {
+            enemyHp -= hero.baseDamage
+            println("HP MUSUH : $enemyHp")
+
+            if (enemyHp > 0) {
+                val enemyDamage = (10..20).random()
+                hero.takeDamage(enemyDamage)
+                println("Hp HERO : ${hero.hp}")
+            }
+        } else {
+            println("KABUR")
+            break
+        }
+    }
+}
