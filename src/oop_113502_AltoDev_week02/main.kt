f71606b0ca203463a7ea96f28aa614d563eb6fc5
AltoDev <hitokawiswara@gmail.com>
2026-02-10 15:40:10 +0700

    week02: implement selection logic for constructors

--- src/oop_113502_AltoDev_week02/main.kt
+++ src/oop_113502_AltoDev_week02/main.kt
@@ -7,11 +7,28 @@ fun main(){
 
     println("---Aplikasi PMB UMN---")
     print("Masukkan Nama:")
-    val name = scanner.nextline()
+    val name = scanner.nextLine()
     print("Masukkan NIM (wajib 5 karakter):")
     val nim = scanner.next()
 
     scanner.nextLine()
+
+    print("pilih jalur (1.reguler, 2.umum :")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1){
+        print("Masukkan jurusan : ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name,nim,major)
+        println("Terdaftar di: ${s1.major} dengan gpa awal ${s1.gpa}")
+    }else if (type == 2){
+        val s2 = Student(name,nim)
+        println("Terdaftar di : ${s2.major} dengan gpa awal ${s2.gpa}")
+    }else {
+        println("pilihan gada, gagal")
+    }
 
     if(nim.length != 5){
         println("Error: pendaftaran dibatalkan. nim harus 5 karakter")
