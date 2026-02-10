913f1d306c00b2b440bd56decc596902c757aeb6
AltoDev <hitokawiswara@gmail.com>
2026-02-10 15:50:46 +0700

    Week02: (task 1) implement library input system

---
+++ src/oop_113502_AltoDev_week02/maintugas.kt
@@ -0,0 +1,24 @@
+package oop_113502_AltoDev_week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Masukkan Judul:")
+    val title = scanner.nextLine()
+
+    println("Masukkan Peminjam:")
+    val borrower = scanner.nextLine()
+
+    println("Masukkan Lama Pinjam:")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("Denda: ${loan.calculateFine()}")
+}
