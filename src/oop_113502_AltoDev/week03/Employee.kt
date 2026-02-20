7c51e07c6885356024225def88ae3865191ab94b
AltoDev <hitokawiswara@gmail.com>
2026-02-20 15:03:40 +0700

    Week03: test all encapsulation logic in main

--- src/oop_113502_AltoDev/week03/Employee.kt
+++ src/oop_113502_AltoDev/week03/Employee.kt
@@ -1,5 +1,7 @@
 package oop_113502_AltoDev.week03
 
+import kotlin.times
+
 class Employee(val name:String){
     var salary: Int = 0
 
@@ -11,21 +13,29 @@ class Employee(val name:String){
             field = value
         }
     }
-}
 
-private var performanceRating: Int = 3
+    private var performanceRating: Int = 3
 
-fun increasePerformance(){
-    performanceRating++
-    println("Kinerja $name meningkat! Rating: $performanceRating")
-}
+    fun increasePerformance(){
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
 
-fun printStatus(){
-    println("Karyawan: $name, Rating: $performanceRating")
+    fun printStatus(){
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
+
+    val tax: Double
+        get() = salary *0.1
+
 }
 
 fun main(){
     val e = Employee("Budi")
-    println("---Test Error---")
-    e.salary = 5000
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+    println("Pajak yang harus dibayar ${e.tax}")
 }
