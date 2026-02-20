12823c99cbfab6cb8428440f85e99af7a50d4408
AltoDev <hitokawiswara@gmail.com>
2026-02-20 14:57:20 +0700

    week03: simulate stack overflow error in setter

--- src/oop_113502_AltoDev/week03/Employee.kt
+++ src/oop_113502_AltoDev/week03/Employee.kt
@@ -2,4 +2,15 @@ package oop_113502_AltoDev.week03
 
 class Employee(val name:String){
     var salary: Int = 0
+
+    set(value) {
+        println("Mencoba set gaji ke: $value")
+        this.salary = value
+    }
+}
+
+fun main(){
+    val e = Employee("Budi")
+    println("---Test Error---")
+    e.salary = 5000
 }
