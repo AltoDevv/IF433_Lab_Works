dae111cfee5dbc455b6fac720254e00d486a564b
AltoDev <hitokawiswara@gmail.com>
2026-02-24 15:00:17 +0700

    week04: test vechile and car in main

---
+++ src/oop_113502_AltoDev/week04/main.kt
@@ -0,0 +1,14 @@
+package oop_113502_AltoDev.week04
+
+fun main(){
+    println("--- Testing Vechile ---")
+    val generalVechile = Vechile("Sepeda onthel")
+    generalVechile.honk()
+    generalVechile.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
