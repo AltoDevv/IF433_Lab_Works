d84b6dbef6152db3653be01f3482c7def6aa8425
AltoDev <hitokawiswara@gmail.com>
2026-03-10 15:27:03 +0700

    week06: (task) create smartcctv with compund interface

---
+++ src/oop_113502_AltoDev/week06/SmartCCTV.kt
@@ -0,0 +1,21 @@
+package oop_113502_AltoDev.week06
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("$name dinyalakan.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("$name dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("$name mulai merekam.")
+    }
+}
