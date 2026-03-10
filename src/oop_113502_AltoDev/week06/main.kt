e0671a696dd93fdc94e31cc55879d8062bcb05a0
AltoDev <hitokawiswara@gmail.com>
2026-03-10 15:30:04 +0700

    week06:(task) instantiate smart devices in main

--- src/oop_113502_AltoDev/week06/main.kt
+++ src/oop_113502_AltoDev/week06/main.kt
@@ -6,6 +6,19 @@ fun processCheckout(method: PaymentMethod, amount: Double){
 }
 
 fun main(){
+
+    //tugas
+    val lamp = SmartLamp("1", "Lampu Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    //tutorial
     val myWatch = Smartwatch()
     myWatch.showTime()
 
