3f6a65df08496113c09924f43da3c9f2630ef470
AltoDev <hitokawiswara@gmail.com>
2026-03-10 14:50:28 +0700

    week05:(task 2) create creditcard class

---
+++ src/oop_113502_AltoDev/week05/CreditCard.kt
@@ -0,0 +1,18 @@
+package oop_113502_AltoDev.week05
+
+class CreditCard(accountName: String, var limit: Double) : PaymentMethod(accountName) {
+
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double): Double {
+
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("$accountName berhasil bayar dengan kartu kredit: $amount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit.")
+        }
+
+    }
+
+}
