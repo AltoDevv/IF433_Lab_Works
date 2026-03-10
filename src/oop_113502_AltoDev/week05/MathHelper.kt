b3fb450b0e9b4cd1fd28c48b21c717bc99546531
AltoDev <hitokawiswara@gmail.com>
2026-03-10 14:43:09 +0700

    week05: (task 1) create mathelper class with overloading

---
+++ src/oop_113502_AltoDev/week05/MathHelper.kt
@@ -0,0 +1,14 @@
+package oop_113502_AltoDev.week05
+
+class MathHelper {
+    fun hitungluas(sisi: Int):Int {
+        return sisi*sisi
+    }
+
+    fun hitungluas(panjang: Int, lebar: Int):Int {
+        return panjang*lebar
+    }
+    fun hitungluas(jarijari: Double): Double {
+        return jarijari*3.14*jarijari
+    }
+}
