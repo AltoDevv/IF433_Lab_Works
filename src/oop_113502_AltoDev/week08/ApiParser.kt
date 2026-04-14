f8b227ef8329f6824c88b9dcb633dd8e611b5534
AltoDev <hitokawiswara@gmail.com>
2026-04-14 15:25:10 +0700

    week08:(task) implement robust ApiParser using elvis and requirenotnull

---
+++ src/oop_113502_AltoDev/week08/ApiParser.kt
@@ -0,0 +1,31 @@
+package oop_113502_AltoDev.week08
+
+class apiparser{
+    fun parseproduct(rawjson: Map<String, Any?>): product?{
+        val id = requireNotNull(rawjson["id"] as? String){
+            "api invalid: missing id"
+        }
+
+        val name = requireNotNull(rawjson["name"] as? String){
+            "api invalid: missing name"
+        }
+
+        val type = rawjson["type"] as? String
+
+        return when(type){
+            "electronic"->{
+                val warranty = (rawjson["warranty"] as? Int) ?: 12
+                electronic(id,name,warranty)
+            }
+
+            "clothing"->{
+                val size = (rawjson["size"] as? String) ?: "all size"
+                clothing(id,name,size)
+            }
+
+            else -> null
+
+        }
+
+    }
+}
