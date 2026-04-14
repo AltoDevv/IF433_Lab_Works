ee025887a287693cc8d2c87b6779978b10435d8d
AltoDev <hitokawiswara@gmail.com>
2026-04-14 15:26:32 +0700

    week08: (task) create javapaymentservice for payment interop

---
+++ src/oop_113502_AltoDev/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_113502_AltoDev.week08;
+
+public class JavaPaymentService {
+    public static String processpayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
