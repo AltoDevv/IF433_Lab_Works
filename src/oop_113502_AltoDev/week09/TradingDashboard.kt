249c9790ba94f1ae60807b703929ae2511568be1
AltoDevv <altodev20@gmail.com>
2026-04-21 15:17:17 +0700

    week09:(task) populate mock trade history lsit

---
+++ src/oop_113502_AltoDev/week09/TradingDashboard.kt
@@ -0,0 +1,17 @@
+package oop_113502_AltoDev.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BNBUSDT", "LONG",  30,  12.4,  "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 12,  -3.8,  "CLOSED"),
+        TradeLog("ADAUSDT", "LONG",  18,  7.6,   "OPEN"),
+        TradeLog("DOGEUSDT", "SHORT", 25, -9.1,  "CLOSED"),
+        TradeLog("AVAXUSDT", "LONG",  10,  5.3,  "OPEN"),
+        TradeLog("MATICUSDT", "SHORT", 8,  2.2,  "CLOSED"),
+        TradeLog("DOTUSDT", "LONG",   14, -4.7,  "CLOSED")
+    ).shuffled()
+
+    tradeHistory.forEach {
+        println(it)
+    }
+}
