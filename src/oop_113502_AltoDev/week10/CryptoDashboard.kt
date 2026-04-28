1360d1cc6b312a97ec2c26b0b8614e7da5c71e17
AltoDevv <altodev20@gmail.com>
2026-04-28 15:49:37 +0700

    week10: (Task) print dashboard using foreach on generic list

--- src/oop_113502_AltoDev/week10/CryptoDashboard.kt
+++ src/oop_113502_AltoDev/week10/CryptoDashboard.kt
@@ -9,4 +9,10 @@ fun main() {
     coinRepo.add(Coin("SLA",510.00))
 
     val response = ApiResponse("200 ok", coinRepo.getAll())
+
+    println("\nStatus Respons: ${response.status}")
+    println("\n--- DAFTAR KOIN ---")
+    response.data.forEach { coin ->
+        println("Koin: ${coin.name} | Balance: ${coin.balance}")
+    }
 }
