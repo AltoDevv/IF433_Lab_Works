58cf98b21e10372445459eecd07a9d18412aa515
AltoDevv <altodev20@gmail.com>
2026-05-05 15:36:00 +0700

    week11: (task) configure philips wiz using apply

--- src/oop_113502_AltoDev/week11/SmartHomeDashboard.kt
+++ src/oop_113502_AltoDev/week11/SmartHomeDashboard.kt
@@ -3,4 +3,11 @@ package oop_113502_AltoDev.week11
 fun main() {
     println("=== SMART HOME DASHBOARD ===")
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
