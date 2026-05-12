7af4f2bd0687a4b5ca3146e1ceefee48d0693f45
AltoDevv <altodev20@gmail.com>
2026-05-12 15:32:47 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_113502_AltoDev/week12/SmartFeeder.kt
+++ src/oop_113502_AltoDev/week12/SmartFeeder.kt
@@ -3,4 +3,7 @@ package oop_113502_AltoDev.week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
     return TODO("Provide the return value")
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
